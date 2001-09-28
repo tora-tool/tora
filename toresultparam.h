1c05cb86bf9231a4cdc4c030b00146acfc1d8392
hpj <hpj@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2001-09-28 07:55:56 +0000

    *** empty log message ***

---
+++ toresultparam.h
@@ -0,0 +1,95 @@
+//***************************************************************************
+/*
+ * TOra - An Oracle Toolkit for DBA's and developers
+ * Copyright (C) 2000-2001,2001 GlobeCom AB
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation;  only version 2 of
+ * the License is valid for this program.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *      As a special exception, you have permission to link this program
+ *      with the Oracle Client libraries and distribute executables, as long
+ *      as you follow the requirements of the GNU GPL in regard to all of the
+ *      software in the executable aside from Oracle client libraries. You
+ *      are also allowed to link this program with the Qt Non Commercial for
+ *      Windows.
+ *
+ *      Specifically you are not permitted to link this program with the
+ *      Qt/UNIX or Qt/Windows products of TrollTech. And you are not
+ *      permitted to distribute binaries compiled against these libraries
+ *      without written consent from GlobeCom AB. Observe that this does not
+ *      disallow linking to the Qt Free Edition.
+ *
+ * All trademarks belong to their respective owners.
+ *
+ ****************************************************************************/
+
+#ifndef __TORESULTPARAM_H
+#define __TORESULTPARAM_H
+
+#include <qvbox.h>
+#include <map>
+#include "toresult.h"
+
+class QLineEdit;
+class toResultView;
+
+/** This widget displays the parameters of a session and lets
+ * you edit them.
+ */
+
+class toResultParam : public QVBox, public toResult {
+  Q_OBJECT
+
+  int LastItem;
+  QString LastValue;
+  std::map<int,QString> NewValues;
+
+  toResultView *Params;
+  QLineEdit *Value;
+
+  void saveChange();
+public:
+  /** Create the widget.
+   * @param parent Parent widget.
+   * @param name Name of widget.
+   */
+  toResultParam(QWidget *parent,const char *name=NULL);
+
+  virtual bool canHandle(toConnection &conn);
+private slots:
+  void generateFile(void);
+  void applySession(void);
+  void applySystem(void);
+  void changeItem(void);
+public slots:
+  virtual void query(const QString &sql,const toQList &param);
+  /** Reimplemented for internal reasons.
+   */
+  virtual void refresh(void)
+  { toResult::refresh(); }
+  /** Reimplemented for internal reasons.
+   */
+  virtual void changeParams(const QString &Param1)
+  { toResult::changeParams(Param1); }
+  /** Reimplemented For internal reasons.
+   */
+  virtual void changeParams(const QString &Param1,const QString &Param2)
+  { toResult::changeParams(Param1,Param2); }
+  /** Reimplemented for internal reasons.
+   */
+  virtual void changeParams(const QString &Param1,const QString &Param2,const QString &Param3)
+  { toResult::changeParams(Param1,Param2,Param3); }
+};
+
+#endif
