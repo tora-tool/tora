9bc0f2b2d162ed2b6ac5921c9e59b914324be5a2
hpj <hpj@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2003-02-02 00:36:53 +0000

    *** empty log message ***

--- rpm/releasetora.sh
+++ rpm/releasetora.sh
@@ -28,16 +28,7 @@ then
     TYPE=-$3
 fi
 
-if rpm -qa | grep 'libc-2\.2' > /dev/null
-then
-    GLIBC=glibc22
-    ORA=9
-    echo Detected glibc-2.2
-else
-    GLIBC=glibc21
-    ORA=8
-    echo Detected glibc-2.1
-fi
+GLIBC=glibc22
 
 rm -rf `find tora -name CVS`
 touch tora/* tora/*/*
@@ -48,10 +39,9 @@ ln -sf `pwd`/../tora$TYPE-$1.tar.gz /usr/src/RPM/SOURCES/tora-$1.tar.gz
 
 QTDIR=/usr/local/qt3
 export QTDIR
-if [ "X$GLIBC" != "Xglibc21" ]
-then
-    export ORACLE_HOME=$ORACLE_HOME/../9.0.1
-fi
+
+export ORA=8
+
 echo Building rpm tora$TYPE-$1-1oracle$ORA.i686.rpm 
 rpm -bb tora-$1/rpm/tora-static.spec
 cp /usr/src/RPM/RPMS/*/tora-$1-1static.*.rpm ../tora$TYPE-$1-1oracle$ORA.i686.rpm 
@@ -65,6 +55,24 @@ echo Packing tora$TYPE-$1-oracle$ORA.tar.gz
 tar czf $SRCPWD/tora$TYPE-$1-oracle$ORA.tar.gz tora-$1/tora tora-$1/README tora-$1/LICENSE.txt \
         tora-$1/BUGS tora-$1/NEWS tora-$1/INSTALL tora-$1/TODO tora-$1/templates/sqlfunctions.tpl \
 	tora-$1/icons/tora.xpm tora-$1/icons/toramini.xpm tora-$1/help
+
+export ORACLE_HOME=$ORACLE_HOME/../9.0.1
+export ORA=9
+
+echo Building rpm tora$TYPE-$1-1oracle$ORA.i686.rpm 
+rpm -bb tora-$1/rpm/tora-static.spec
+cp /usr/src/RPM/RPMS/*/tora-$1-1static.*.rpm ../tora$TYPE-$1-1oracle$ORA.i686.rpm 
+
+cd /usr/src/RPM/BUILD/tora-$1
+strip tora-static
+mv tora-static tora
+cd ..
+echo Packing tora$TYPE-$1-oracle$ORA.tar.gz
+
+tar czf $SRCPWD/tora$TYPE-$1-oracle$ORA.tar.gz tora-$1/tora tora-$1/README tora-$1/LICENSE.txt \
+        tora-$1/BUGS tora-$1/NEWS tora-$1/INSTALL tora-$1/TODO tora-$1/templates/sqlfunctions.tpl \
+	tora-$1/icons/tora.xpm tora-$1/icons/toramini.xpm tora-$1/help
+
 
 cd $SRCPWD
 rm -rf release
