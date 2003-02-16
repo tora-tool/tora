481201c2c6f7534eb6b16512500fb5c47948ad17
hpj <hpj@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2003-02-16 10:20:07 +0000

    *** empty log message ***

--- toconf.h
+++ toconf.h
@@ -156,7 +156,7 @@
 #  define DEFAULT_PLUGIN_DIR ""
 #endif
 // Version of TOra, must also change rpm files
-#define TOVERSION               "1.3.9.1"
+#define TOVERSION               "1.3.9.2"
 // Stable or development version of TOra
 #define TOTYPE			"DEVELOP"
 
