74ff91ae7427a170b4461db1338269dc9d8084eb
hpj <hpj@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2003-07-22 21:25:35 +0000

    *** empty log message ***

--- rpm/releasetora.sh
+++ rpm/releasetora.sh
@@ -17,11 +17,11 @@ cd release
 if [ "X$2" = "X" ]
 then
     echo Releasing head
-    cvs -z3 -d:pserver:anonymous@cvs.tora.sourceforge.net:/cvsroot/tora co tora
+    cvs -d`cat ../tora/CVS/Root` co tora
     TYPE=-beta
 else
     echo Releasing branch $2
-    cvs -z3 -d:pserver:anonymous@cvs.tora.sourceforge.net:/cvsroot/tora co -r$2 tora
+    cvs -d`cat ../tora/CVS/Root` co tora
 fi
 if [ "X$3" != "X" ]
 then
@@ -40,36 +40,17 @@ ln -sf `pwd`/../tora$TYPE-$1.tar.gz /usr/src/RPM/SOURCES/tora-$1.tar.gz
 QTDIR=/usr/local/qt3
 export QTDIR
 
-export ORA=8
-
-echo Building rpm tora$TYPE-$1-1oracle$ORA.i686.rpm 
+echo Building rpm tora$TYPE-$1-1underscore.i686.rpm 
 rpm -bb tora-$1/rpm/tora-static.spec
-cp /usr/src/RPM/RPMS/*/tora-$1-1static.*.rpm $SRCPWD/tora$TYPE-$1-1oracle$ORA.i686.rpm 
+cp /usr/src/RPM/RPMS/*/tora-$1-1static.*.rpm $SRCPWD/tora$TYPE-$1-1underscore.i686.rpm 
 
 cd /usr/src/RPM/BUILD/tora-$1
 strip tora-static
 mv tora-static tora
 cd ..
-echo Packing tora$TYPE-$1-oracle$ORA.tar.gz
+echo Packing tora$TYPE-$1-underscore.tar.gz
 
-tar czf $SRCPWD/tora$TYPE-$1-oracle$ORA.tar.gz tora-$1/tora tora-$1/README tora-$1/LICENSE.txt \
-        tora-$1/BUGS tora-$1/NEWS tora-$1/INSTALL tora-$1/TODO tora-$1/templates/sqlfunctions.tpl \
-	tora-$1/icons/tora.xpm tora-$1/icons/toramini.xpm tora-$1/help
-
-export ORACLE_HOME=$ORACLE_HOME/../9.0.1
-export ORA=9
-
-echo Building rpm tora$TYPE-$1-1oracle$ORA.i686.rpm 
-rpm -bb tora-$1/rpm/tora-static.spec
-cp /usr/src/RPM/RPMS/*/tora-$1-1static.*.rpm $SRCPWD/tora$TYPE-$1-1oracle$ORA.i686.rpm 
-
-cd /usr/src/RPM/BUILD/tora-$1
-strip tora-static
-mv tora-static tora
-cd ..
-echo Packing tora$TYPE-$1-oracle$ORA.tar.gz
-
-tar czf $SRCPWD/tora$TYPE-$1-oracle$ORA.tar.gz tora-$1/tora tora-$1/README tora-$1/LICENSE.txt \
+tar czf $SRCPWD/tora$TYPE-$1-underscore.tar.gz tora-$1/tora tora-$1/README tora-$1/LICENSE.txt \
         tora-$1/BUGS tora-$1/NEWS tora-$1/INSTALL tora-$1/TODO tora-$1/templates/sqlfunctions.tpl \
 	tora-$1/icons/tora.xpm tora-$1/icons/toramini.xpm tora-$1/help
 
