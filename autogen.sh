e8b3894855123626aa2b19dd3f5a024bac1a119c
mrjohnson0 <mrjohnson0@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2005-03-20 23:52:46 +0000

    small improvements, use glibtoolize on mac

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,35 @@
 #!/bin/sh
-autoheader && aclocal-1.6 && libtoolize --automake && autoconf && automake-1.6 -a
+
+me=$0
+
+err() {
+    echo "$me: $1"
+    exit 1
+}
+
+# these programs don't always print their program names.
+run() {
+    echo "$me: running $1 ..."
+    $*
+    if test "$?" != "0"; then
+        err "$1 failed. exit."
+    fi
+}
+
+libtool() {
+    opts="--automake --copy"
+
+    if glibtoolize --version >/dev/null 2>&1; then
+        run glibtoolize $opts
+    elif libtoolize --version >/dev/null 2>&1; then
+        run libtoolize $opts
+    else
+        err "libtoolize not found"
+    fi
+}
+
+run autoheader
+run aclocal-1.6
+run autoconf
+libtool
+run automake-1.6 -a
