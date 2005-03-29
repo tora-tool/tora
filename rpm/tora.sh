60da86b40d3a0e1ad778235c89f81367d37431b8
winterchild <winterchild@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2005-03-29 19:15:47 +0000

    minor tora.sh update to work when used without Oracle

--- rpm/tora.sh
+++ rpm/tora.sh
@@ -2,9 +2,9 @@
 
 if [ "X$ORACLE_HOME" = "X" ]
 then
-    echo "Environment variable ORACLE_HOME not set, check your Oracle installation"
+    echo "WARNING: Environment variable ORACLE_HOME not set, check your Oracle installation"
 else
     LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME/lib
     export LD_LIBRARY_PATH
-    tora.real
 fi
+tora.real
