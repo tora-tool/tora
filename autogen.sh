b649e9ae5c343faea3abaae5b9a74b97517ff278
mrjohnson0 <mrjohnson0@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2005-05-03 05:00:21 +0000

    a quick hack to hunt for version numbers

--- autogen.sh
+++ autogen.sh
@@ -7,12 +7,44 @@ err() {
     exit 1
 }
 
-# these programs don't always print their program names.
+# these programs don't always print their program names. does some
+# pretty output, finds version numbers.
+#
+# $1 - prog name
+# $2 - arguments
+# $3 - preferred version, optional, major
+# $4 - preferred version, optional, minor
 run() {
-    echo "$me: running $1 ..."
-    $*
+    prog=$1
+    args=$2
+    major=$3
+    minor=$4
+
+    if test "${major}x" != "x"; then
+        if test "${minor}x" = "x"; then
+            minor=0
+        fi
+
+        while expr $major \< 10 >/dev/null; do
+            # find a version number greater than param
+            ${prog}-${major}.${minor} --version >/dev/null 2>&1
+            if test "$?" == "0"; then
+                prog=${prog}-${major}.${minor}
+                break
+            fi
+
+            minor=`expr $minor + 1`
+            if expr $minor \> 15 >/dev/null; then
+                minor=0
+                major=`expr $major + 1`
+            fi
+        done
+    fi
+
+    echo "$me: running $prog$ver $args..."
+    $prog $args
     if test "$?" != "0"; then
-        err "$1 failed. exit."
+        err "$prog failed. exit."
     fi
 }
 
@@ -29,7 +61,7 @@ libtool() {
 }
 
 run autoheader
-run aclocal-1.6
+run aclocal '' 1 5
 run autoconf
 libtool
-run automake-1.6 -a
+run automake -a 1 5
