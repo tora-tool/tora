9172463dc30df7b4755bcca6bfe57662ab392b20
mrjohnson0 <mrjohnson0@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2007-02-13 05:53:30 +0000

    fix bug 1655906
    because using QString::arg.arg().arg() is Retarded

--- src/tolistviewformattercsv.cpp
+++ src/tolistviewformattercsv.cpp
@@ -1,39 +1,39 @@
 /*****
-*
-* TOra - An Oracle Toolkit for DBA's and developers
-* Copyright (C) 2003-2005 Quest Software, Inc
-* Portions Copyright (C) 2005 Other Contributors
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation;  only version 2 of
-* the License is valid for this program.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*
-*      As a special exception, you have permission to link this program
-*      with the Oracle Client libraries and distribute executables, as long
-*      as you follow the requirements of the GNU GPL in regard to all of the
-*      software in the executable aside from Oracle client libraries.
-*
-*      Specifically you are not permitted to link this program with the
-*      Qt/UNIX, Qt/Windows or Qt Non Commercial products of TrollTech.
-*      And you are not permitted to distribute binaries compiled against
-*      these libraries without written consent from Quest Software, Inc.
-*      Observe that this does not disallow linking to the Qt Free Edition.
-*
-*      You may link this product with any GPL'd Qt library such as Qt/Free
-*
-* All trademarks belong to their respective owners.
-*
-*****/
+ *
+ * TOra - An Oracle Toolkit for DBA's and developers
+ * Copyright (C) 2003-2005 Quest Software, Inc
+ * Portions Copyright (C) 2005 Other Contributors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation;  only version 2 of
+ * the License is valid for this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *      As a special exception, you have permission to link this program
+ *      with the Oracle Client libraries and distribute executables, as long
+ *      as you follow the requirements of the GNU GPL in regard to all of the
+ *      software in the executable aside from Oracle client libraries.
+ *
+ *      Specifically you are not permitted to link this program with the
+ *      Qt/UNIX, Qt/Windows or Qt Non Commercial products of TrollTech.
+ *      And you are not permitted to distribute binaries compiled against
+ *      these libraries without written consent from Quest Software, Inc.
+ *      Observe that this does not disallow linking to the Qt Free Edition.
+ *
+ *      You may link this product with any GPL'd Qt library such as Qt/Free
+ *
+ * All trademarks belong to their respective owners.
+ *
+ *****/
 
 #include "tolistviewformattercsv.h"
 #include "tolistviewformatterfactory.h"
@@ -46,11 +46,12 @@
 
 namespace
 {
-   toListViewFormatter* createCSV()
-   {
+    toListViewFormatter* createCSV()
+    {
         return new toListViewFormatterCSV();
-   }
-   const bool registered = toListViewFormatterFactory::Instance().Register(toListViewFormatterIdentifier::CSV, createCSV);
+    }
+    const bool registered = toListViewFormatterFactory::Instance().Register(
+        toListViewFormatterIdentifier::CSV, createCSV);
 }
 
 
@@ -72,7 +73,6 @@ QString toListViewFormatterCSV::QuoteString(const QString &str)
 
 QString toListViewFormatterCSV::getFormattedString(toListView& tListView)
 {
-    std::cout << "Yep,  im FORMATTERCSV\n";
     int column_count = tListView.columns();
     QString separator = tListView.getSep();
     QString delimiter = tListView.getDel();
@@ -91,25 +91,23 @@ QString toListViewFormatterCSV::getFormattedString(toListView& tListView)
             else
                 bgcolor = QString::null;
             for (int j = 0;j < column_count;j++)
-                    output += QString::fromLatin1("%1%2%3%4").
-                              arg(delimiter).
-                              arg(QuoteString((tListView.header())->label(j))).
-                              arg(delimiter).
-                              arg(separator);
+                output += QString::fromLatin1("%1%2%3%4").
+                    arg(delimiter).
+                    arg(QuoteString((tListView.header())->label(j))).
+                    arg(delimiter).
+                    arg(separator);
             if (output.length() > 0 )
                 output = output.left(output.length() - separator.length());
-#ifdef WIN32
 
+#ifdef WIN32
             output += "\r\n";
 #else
-
             output += "\n";
 #endif
-            
         }
 
              
-             QListViewItem *next = NULL;
+        QListViewItem *next = NULL;
 
         for (QListViewItem *item = tListView.firstChild();item;item = next)
         {
@@ -136,19 +134,19 @@ QString toListViewFormatterCSV::getFormattedString(toListView& tListView)
                     else
                         text = item->text(i);
 
-                        line += indent;
-                        line += QString::fromLatin1("%1%2%3%4").
-                                arg(delimiter).
-                                arg(QuoteString(text)).
-                                arg(delimiter).
-                                arg(separator);
-               }
-                    line = line.left(line.length() - separator.length());
-#ifdef WIN32
+                    line += indent;
 
+                    line += QString::fromLatin1("%1%2%3%4").
+                        arg(delimiter,
+                            QuoteString(text),
+                            delimiter,
+                            separator);
+                }
+                line = line.left(line.length() - separator.length());
+
+#ifdef WIN32
                 line += "\r\n";
 #else
-
                 line += "\n";
 #endif
 
@@ -156,7 +154,7 @@ QString toListViewFormatterCSV::getFormattedString(toListView& tListView)
             }
 
             if (item->firstChild())
-            {            
+            {
                 indent += QString::fromLatin1(" ");
                 next = item->firstChild();
             }
