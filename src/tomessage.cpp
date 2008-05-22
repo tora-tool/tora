b99382191167d7ba32c3a8d98e11a601f438509c
subik <subik@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2008-05-22 13:38:57 +0000

    missing files. sorry.

---
+++ src/tomessage.cpp
@@ -0,0 +1,81 @@
+/*****
+*
+* TOra - An Oracle Toolkit for DBA's and developers
+* Copyright (C) 2003-2005 Quest Software, Inc
+* Portions Copyright (C) 2005 Other Contributors
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation;  only version 2 of
+* the License is valid for this program.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*      As a special exception, you have permission to link this program
+*      with the Oracle Client libraries and distribute executables, as long
+*      as you follow the requirements of the GNU GPL in regard to all of the
+*      software in the executable aside from Oracle client libraries.
+*
+*      Specifically you are not permitted to link this program with the
+*      Qt/UNIX, Qt/Windows or Qt Non Commercial products of TrollTech.
+*      And you are not permitted to distribute binaries compiled against
+*      these libraries without written consent from Quest Software, Inc.
+*      Observe that this does not disallow linking to the Qt Free Edition.
+*
+*      You may link this product with any GPL'd Qt library such as Qt/Free
+*
+* All trademarks belong to their respective owners.
+*
+*****/
+
+#include <QPushButton>
+#include <QDateTime>
+#include <QSettings>
+
+#include "tomessage.h"
+
+
+toMessage::toMessage(QWidget * parent, Qt::WindowFlags f)
+    : QDialog(parent, f)
+{
+    setupUi(this);
+    setModal(false);
+    Message->setReadOnly(true);
+
+    QSettings s;
+    s.beginGroup("toMessage");
+    restoreGeometry(s.value("geometry", QByteArray()).toByteArray());
+    s.endGroup();
+
+    connect(buttonBox->button(QDialogButtonBox::Close),
+            SIGNAL(clicked()), this, SLOT(hide()));
+    connect(buttonBox->button(QDialogButtonBox::Reset),
+            SIGNAL(clicked()), Message, SLOT(clear()));
+}
+
+toMessage::~toMessage()
+{
+    QSettings s;
+    s.beginGroup("toMessage");
+    s.setValue("geometry", saveGeometry());
+    s.endGroup();
+}
+
+void toMessage::appendText(const QString & text)
+{
+    if (!isVisible())
+        show();
+
+    if (Message->lines() != 1)
+        Message->append("\n\n");
+    Message->append(QDateTime::currentDateTime().toString(Qt::DefaultLocaleShortDate) + "\n");
+    Message->append(text);
+    Message->ensureLineVisible(Message->lines());
+}
