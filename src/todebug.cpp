2612b2a97e9954ea6287732ea9f6772e74ec5cb2
mrjohnson0 <mrjohnson0@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2008-08-19 13:57:15 +0000

    open package and body

--- src/todebug.cpp
+++ src/todebug.cpp
@@ -2393,10 +2393,9 @@ void toDebug::changePackage(const QModelIndex &current, const QModelIndex &previ
             return;
         ctype = ctype.toUpper();
 
-        if (ctype == "PACKAGE" || ctype == "TYPE")
-            ctype += " BODY";
-
         viewSource(Schema->currentText(), item->text(0), ctype, 0);
+        if (ctype == "PACKAGE" || ctype == "TYPE")
+            viewSource(Schema->currentText(), item->text(0), ctype + " BODY", 0);
     }
 #ifdef AUTOEXPAND
     else if (item && !item->parent())
