6691a970f12f2baa0b8715f0247cbe326c8f830b
nneul <nneul@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2008-09-14 21:22:53 +0000

    update refresh script with c-style comments and run refresh files

--- refresh-copyright-and-license.pl
+++ refresh-copyright-and-license.pl
@@ -40,11 +40,58 @@ print COPYOUT $tmp;
 close(COPYOUT);
 
 #
+# Generate the replacement C-style copyright
+#
+my $c_style = "\n/* BEGIN_COMMON_COPYRIGHT_HEADER\n *\n";
+foreach my $ln ( split( /[\r\n]/, $copy_text ) ) {
+    $c_style .= " * $ln\n";
+}
+$c_style .= " *\n * END_COMMON_COPYRIGHT_HEADER */\n\n";
+
+#
 # Update all the C-style copyright text entries
 #
 foreach $file ( glob("src/*.h"), glob("src/*/*.h"), glob("src/*.cpp"),
     glob("src/*/*.cpp") )
 {
+    next if ( $file eq "src/LICENSE.h" );
+    next if ( $file eq "src/COPYRIGHT.h" );
+
     print "Refreshing $file.\n";
+
+    local ($/);
+    my $filetext = do { local ($/); my $fh; open( $fh, "<", $file ); <$fh> };
+    $filetext =~
+s|\s*/\*\s*BEGIN_COMMON_COPYRIGHT_HEADER.*END_COMMON_COPYRIGHT_HEADER\s*\*/\s*|$c_style|sx;
+
+    open( OUT, ">", $file );
+    print OUT $filetext;
+    close(OUT);
 }
 
+#!/usr/bin/perl
+
+$/ = undef;
+
+open( IN, "/tmp/copy-chunk" );
+my $lic = join( "", <IN> );
+close(IN);
+my $qlic = quotemeta($lic);
+
+#print "QLIC = $qlic\n";
+
+$newlic = "/* BEGIN_COMMON_COPYRIGHT_HEADER 
+* END_COMMON_COPYRIGHT_HEADER */
+";
+
+foreach $file (@ARGV) {
+    open( IN, $file );
+    my $filetext = join( "", <IN> );
+    close(IN);
+
+    $filetext =~ s/$qlic/$newlic/sx;
+
+    open( OUT, ">$file" );
+    print OUT $filetext;
+    close(OUT);
+}
