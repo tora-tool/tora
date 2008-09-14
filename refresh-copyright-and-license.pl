fe9890a633ccf978995b4dd0ba0ad1b391ad9ac7
nneul <nneul@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2008-09-14 21:48:27 +0000

    don't overwrite if nothing has changed

--- refresh-copyright-and-license.pl
+++ refresh-copyright-and-license.pl
@@ -2,22 +2,47 @@
 $| = 1;
 
 #
+# Util routines
+#
+sub write_if_changed {
+    my $origtext = shift;
+    my $curtext  = shift;
+    my $file     = shift;
+
+    if ( $origtext ne $filetext ) {
+        print "Refreshing $file with new copyright text.";
+        open( OUT, ">", $file );
+        print OUT $filetext;
+        close(OUT);
+    }
+    else {
+        print "Skipping $file, no changes.\n";
+    }
+}
+
+sub slurp {
+    my $file = shift;
+    return do { local ($/); my $fh; open( $fh, $file ); <$fh> };
+}
+
+#
 # Load in master files
 #
-my $lic_text  = do { local ($/); my $fh; open( $fh, "COPYING" );       <$fh> };
-my $copy_text = do { local ($/); my $fh; open( $fh, "copyright.txt" ); <$fh> };
+my $lic_text  = slurp("COPYING");
+my $copy_text = slurp("copyright.txt");
 
-if ( $ARGV[0] eq "--blank" )
-{
+if ( $ARGV[0] eq "--blank" ) {
     shift @ARGV;
     $copy_text = "";
-    $lic_text = "";
+    $lic_text  = "";
 }
 
 #
 # Generate the src/LICENSE.h file
 #
-print "Refreshing src/LICENSE.h\n";
+my $file     = "src/LICENSE.h";
+my $origtext = slurp($file);
+
 my $tmp = "static const char *LicenseText = ";
 $tmp .= "    \"<html><body><pre>\\n\"\n";
 foreach my $ln ( split( /[\r\n]/, $lic_text ) ) {
@@ -26,14 +51,14 @@ foreach my $ln ( split( /[\r\n]/, $lic_text ) ) {
     $tmp .= "    \"$ln\\n\"\n";
 }
 $tmp .= "    \"</pre></body></html>\\n\"\n;";
-open( LICOUT, ">src/LICENSE.h" );
-print LICOUT $tmp;
-close(LICOUT);
+write_if_changed( $origtext, $tmp, $file );
 
 #
 # Generate the src/COPYRIGHT.h file
 #
-print "Refreshing src/COPYRIGHT.h\n";
+my $file     = "src/COPYRIGHT.h";
+my $origtext = slurp($file);
+
 my $tmp = "static const char *CopyrightText = ";
 $tmp .= "    \"<pre>\\n\"\n";
 foreach my $ln ( split( /[\r\n]/, $copy_text ) ) {
@@ -42,9 +67,7 @@ foreach my $ln ( split( /[\r\n]/, $copy_text ) ) {
     $tmp .= "    \"$ln\\n\"\n";
 }
 $tmp .= "    \"</pre>\\n\"\n;";
-open( COPYOUT, ">src/COPYRIGHT.h" );
-print COPYOUT $tmp;
-close(COPYOUT);
+write_if_changed( $origtext, $tmp, $file );
 
 #
 # Generate the replacement C-style copyright
@@ -64,16 +87,13 @@ foreach $file ( glob("src/*.h"), glob("src/*/*.h"), glob("src/*.cpp"),
     next if ( $file eq "src/LICENSE.h" );
     next if ( $file eq "src/COPYRIGHT.h" );
 
-    print "Refreshing $file.\n";
-
     local ($/);
-    my $filetext = do { local ($/); my $fh; open( $fh, "<", $file ); <$fh> };
+    my $filetext = slurp($file);
+    my $origtext = $filetext;
     $filetext =~
 s|\s*/\*\s*BEGIN_COMMON_COPYRIGHT_HEADER.*END_COMMON_COPYRIGHT_HEADER\s*\*/\s*|$c_style|sx;
 
-    open( OUT, ">", $file );
-    print OUT $filetext;
-    close(OUT);
+    write_if_changed( $origtext, $filetext, $file );
 }
 
 #
@@ -88,17 +108,15 @@ $plain_style .= "\nEND_COMMON_COPYRIGHT_HEADER\n\n";
 #
 # Update all the C-style copyright text entries
 #
-foreach $file ("README", "debian/copyright") {
-    print "Refreshing $file.\n";
-
+foreach $file ( "README", "debian/copyright" ) {
     local ($/);
-    my $filetext = do { local ($/); my $fh; open( $fh, "<", $file ); <$fh> };
+    my $filetext = slurp($file);
+    my $origtext = $filetext;
+
     $filetext =~
 s|\s*BEGIN_COMMON_COPYRIGHT_HEADER.*END_COMMON_COPYRIGHT_HEADER\s*|$plain_style|sx;
 
-    open( OUT, ">", $file );
-    print OUT $filetext;
-    close(OUT);
+    write_if_changed( $origtext, $filetext, $file );
 }
 
 #
@@ -113,16 +131,15 @@ $shell_style .= "#\n# END_COMMON_COPYRIGHT_HEADER\n\n";
 #
 # Update all the C-style copyright text entries
 #
-foreach $file ( glob("Makefile*"), glob("*/Makefile*"), glob("*/*/Makefile*") ) {
-    print "Refreshing $file.\n";
-
+foreach $file ( glob("Makefile*"), glob("*/Makefile*"), glob("*/*/Makefile*") )
+{
     local ($/);
-    my $filetext = do { local ($/); my $fh; open( $fh, "<", $file ); <$fh> };
+    my $filetext = slurp($file);
+    my $origtext = $filetext;
+
     $filetext =~
 s|\s*\#\s*BEGIN_COMMON_COPYRIGHT_HEADER.*END_COMMON_COPYRIGHT_HEADER\s*|$shell_style|sx;
 
-    open( OUT, ">", $file );
-    print OUT $filetext;
-    close(OUT);
+    write_if_changed( $origtext, $filetext, $file );
 }
 
