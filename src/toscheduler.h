873fe212c6ecde7722da9a576da3d98ffe50782f
subik <subik@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2009-02-23 14:00:09 +0000

    scheduler/jobs tool initial import - it's disabled from building for now (I'd like just keep the code).

---
+++ src/toscheduler.h
@@ -0,0 +1,96 @@
+
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ *
+ * TOra - An Oracle Toolkit for DBA's and developers
+ * 
+ * Shared/mixed copyright is held throughout files in this product
+ * 
+ * Portions Copyright (C) 2000-2001 Underscore AB
+ * Portions Copyright (C) 2003-2005 Quest Software, Inc.
+ * Portions Copyright (C) 2004-2008 Numerous Other Contributors
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation;  only version 2 of
+ * the License is valid for this program.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ *      As a special exception, you have permission to link this program
+ *      with the Oracle Client libraries and distribute executables, as long
+ *      as you follow the requirements of the GNU GPL in regard to all of the
+ *      software in the executable aside from Oracle client libraries.
+ * 
+ *      Specifically you are not permitted to link this program with the
+ *      Qt/UNIX, Qt/Windows or Qt Non Commercial products of TrollTech.
+ *      And you are not permitted to distribute binaries compiled against
+ *      these libraries. 
+ * 
+ *      You may link this product with any GPL'd Qt library.
+ * 
+ * All trademarks belong to their respective owners.
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
+#ifndef TOSCHEDULER_H
+#define TOSCHEDULER_H
+
+#include "totool.h"
+
+#include "ui_toschedulerui.h"
+
+class toResultCombo;
+class toSchedulerWidget;
+
+
+/*! \brief
+\author Petr Vanek <petr@scribus.info>
+*/
+class toScheduler : public toToolWidget
+{
+    Q_OBJECT
+
+    public:
+        toScheduler(QWidget * parent, toConnection &connection);
+        ~toScheduler();
+
+        /** \brief Inform upstream parent object for what platform
+        is this tool available. It's Oracle only this time.
+        */
+        virtual bool canHandle(toConnection &conn);
+
+    private:
+        int m_predefinedIntervals;
+        QString m_owner;
+        toSchedulerWidget * m_gui;
+
+        toResultCombo *Schema;
+
+    private slots:
+        void changeSchema(const QString &);
+        void refresh();
+        void jobsTableView_selectionChanged();
+        void jobSaveButton_clicked();
+        void jobNew();
+        void jobCopyOf();
+};
+
+
+class toSchedulerWidget : public QWidget, public Ui::toSchedulerUI
+{
+    public:
+        toSchedulerWidget(QWidget * parent)
+            : QWidget(parent)
+        {
+            setupUi(this);
+        };
+};
+
+#endif
