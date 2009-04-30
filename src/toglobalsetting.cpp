c08b98e7d212a4a3bb869b035eea9ba2b035d19b
mrjohnson0 <mrjohnson0@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2009-04-30 06:04:54 +0000

    simplify loop. now it's actually possible to enable the alerts tool

--- src/toglobalsetting.cpp
+++ src/toglobalsetting.cpp
@@ -353,24 +353,19 @@ toToolSetting::toToolSetting(QWidget *parent, const char *name, Qt::WFlags fl)
 
     std::map<QString, toTool *> &tools = toTool::tools();
     Enabled->setSorting(0);
+    ToolsMap tMap(toConfigurationSingle::Instance().tools());
     for (std::map<QString, toTool *>::iterator i = tools.begin();i != tools.end();i++)
     {
         if ((*i).second->menuItem())
         {
             QString menuName = qApp->translate("toTool", (*i).second->menuItem());
             DefaultTool->addItem(menuName);
-            new toTreeWidgetItem(Enabled, menuName, (*i).second->name(), (*i).first);
+            toTreeWidgetItem *item = new toTreeWidgetItem(Enabled,
+                                                          menuName,
+                                                          (*i).second->name(),
+                                                          (*i).first);
+            item->setSelected(tMap[(*i).first]);
         }
-    }
-
-    ToolsMap tMap(toConfigurationSingle::Instance().tools());
-    for (QTreeWidgetItemIterator it(Enabled); (*it); it++)
-    {
-//         QString tmp = (*it)->text(2).toLatin1();
-//         tmp += CONF_TOOL_ENABLE;
-        (*it)->setSelected(tMap[(*it)->text(2)]);
-//         if (!toConfigurationSingle::Instance().globalConfig(tmp, "Yes").isEmpty())
-//             (*it)->setSelected(true);
     }
 
     // set the default tool to prevent overvritting when
