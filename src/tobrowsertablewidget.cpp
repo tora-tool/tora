aca84118cba03ec0f710697c11daeb2213bbbc1b
subik <subik@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2009-05-13 04:52:06 +0000

    previous patch removed

--- src/tobrowsertablewidget.cpp
+++ src/tobrowsertablewidget.cpp
@@ -175,24 +175,14 @@ static toSQL SQLTableConstraint(
        "           AND c.Owner = :f1<char[101]>\n"
        "           AND c.Owner = a.Owner\n"
        "           AND c.Table_Name = :f2<char[101]>\n"
+       "       ORDER BY\n"
+       "           c.constraint_name, a.constraint_name, a.position\n"
        "       ) main,\n"
-       "       (\n"
-       "       SELECT\n"
-       "           owner,\n"
-       "           table_name,\n"
-       "           column_name,\n"
-       "           constraint_name\n"
-       "       FROM\n"
-       "           sys.all_cons_columns\n"
-       "       WHERE\n"
-       "               owner = :f1<char[101]>\n"
-       "           AND table_Name = :f2<char[101]>\n" 
-       "       ) refs\n" 
+       "       sys.all_cons_columns refs\n"
        "   WHERE\n"
        "           main.r_constraint_name = refs.constraint_name (+)\n"
        "       AND main.owner = refs.owner (+)\n"
-       "       AND main.column_name = refs.column_name (+)\n"
-       "   ORDER BY 1\n",
+       "       AND main.column_name = refs.column_name (+)\n",
     "List the constraints on a table",
     "");
 
