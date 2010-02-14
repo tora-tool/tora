7b0fc41dee3841e1607ab942346fab182bd946b0
ibre5041 <ibre5041@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2010-02-14 15:54:47 +0000

    Tora thread safe logger. - each log has its own writter thread, own message queue and buffers - all loggers by default point std::cout(do not use std::cerr) - to implement a ne logger create specialized template function   templ_get_log_ownthread<N> where N is logger number.
      like:
      thread_safe_log templ_get_log_ownthread<5>( int_to_type< 5> * = NULL )
      {
      	static std::ofstream out( "logfile.txt" ); // or any other instance of ostream
     	static internal_thread_safe_log_ownthread log( out);
     	return thread_safe_log( log);
      }
    
      And add our logger into case in the function get_log.
    
    - To use a looger call:
      get_log(N).ts<toDecorator>( __HERE__) << "Thread no: " << threadNumber << "  started." << std::endl;
    
      get_log(N)    - returns Nth instance the class thread_safe_log
      .ts()         - returns reference to thread_safe_log's base basic::ostream
      <toDecorator> - decorator class prints message header like:
                      "----<thread no.> source.cpp:<line> ----"
                      on linux thread numbers have different colors.
      __HERE__      - preprocesor macro __HERE__ represents a character constant
                      containing name of the source, line number and a name of the
                      function/method.
    		  
      You can write instances of QString directly into a stream. Do not use
      construct like: "myQString.toAscii().const_data()". This does not work with
      MSVC. Calling to .toAscii() returns temporary instance of QByteArray, which
      is deallocated just after const_data() returns. Pointer returned from
      const_data() then poinst into annonymous memory.

---
+++ src/tologger.h
@@ -0,0 +1,50 @@
+
+#ifndef __TORA_LOGGER__
+#define __TORA_LOGGER__
+
+#define USE_QT_THREAD_MANAGER 
+#include "ts_log/critical_section.h"
+#include "ts_log/message_handler_log.h"
+#include "ts_log/thread_safe_log.h"
+#include "ts_log/ts_log_utils.h"
+#include "ts_log/decorator.h"
+
+#include <iostream>
+
+typedef Tdecorator<
+	TSLOG_TYPELIST_6(dashDecorator<4>,
+			 tidDecorator<qt_thread_manager>,
+			 charDecorator<' '>,
+			 hereDecorator, 
+			 dashDecorator<4>,
+			 charDecorator<'\n'>
+		)> toDecorator;
+
+template< int idxLog>
+thread_safe_log templ_get_log_ownthread( int_to_type< idxLog> * = NULL )
+{
+	/* static std::ofstream out( "tora_connection.log" ); */
+	static internal_thread_safe_log_ownthread log( std::cout );
+	return thread_safe_log( log);
+}
+
+/*
+thread_safe_log templ_get_log_ownthread<1>( int_to_type< 1> * = NULL )
+{
+	static std::ofstream out( get_out_name< idxLog>( false).c_str() );
+	static internal_thread_safe_log_ownthread log( out);
+	return thread_safe_log( log);
+}
+*/
+
+inline thread_safe_log get_log( int idxLog)
+{
+	switch( idxLog)
+	{
+	case 0: return templ_get_log_ownthread< 0>(); // tooracleconnection log
+	case 1: return templ_get_log_ownthread< 1>(); // exception log
+	default: assert( false);
+	}
+}
+
+#endif
