5d94e182f63a024bf9fa6a2e49d419f2762f3655
subik <subik@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2011-01-06 15:08:45 +0000

    #3047401 - failure to connect changes focus in Connection Manager

--- src/tonewconnection.cpp
+++ src/tonewconnection.cpp
@@ -2,39 +2,39 @@
 /* BEGIN_COMMON_COPYRIGHT_HEADER
  *
  * TOra - An Oracle Toolkit for DBA's and developers
- * 
+ *
  * Shared/mixed copyright is held throughout files in this product
- * 
+ *
  * Portions Copyright (C) 2000-2001 Underscore AB
  * Portions Copyright (C) 2003-2005 Quest Software, Inc.
  * Portions Copyright (C) 2004-2009 Numerous Other Contributors
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation;  only version 2 of
  * the License is valid for this program.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *
  *      As a special exception, you have permission to link this program
  *      with the Oracle Client libraries and distribute executables, as long
  *      as you follow the requirements of the GNU GPL in regard to all of the
  *      software in the executable aside from Oracle client libraries.
- * 
+ *
  *      Specifically you are not permitted to link this program with the
  *      Qt/UNIX, Qt/Windows or Qt Non Commercial products of TrollTech.
  *      And you are not permitted to distribute binaries compiled against
- *      these libraries. 
- * 
+ *      these libraries.
+ *
  *      You may link this product with any GPL'd Qt library.
- * 
+ *
  * All trademarks belong to their respective owners.
  *
  * END_COMMON_COPYRIGHT_HEADER */
@@ -204,7 +204,7 @@ void toNewConnection::writeSettings(bool checkHistory)
     int r = 0;
     Settings.setValue("geometry", saveGeometry());
 
-    Settings.setValue(CONF_PROVIDER_LIST_SORT_OFFSET, 
+    Settings.setValue(CONF_PROVIDER_LIST_SORT_OFFSET,
 		    (Previous->horizontalHeader()->sortIndicatorOrder() == Qt:: AscendingOrder? 1:-1)*
 		    Previous->horizontalHeader()->sortIndicatorSection());
 
@@ -333,12 +333,9 @@ void toNewConnection::loadPrevious(const QModelIndex & current)
     box->setChecked(opt.options.find(box->text()) != opt.options.end());
 }
 
-
 void toNewConnection::done(int r)
 {
-    writeSettings();
-
-    if (!r)
+    if (r == QDialog::Rejected)
     {
         QDialog::done(r);
         return;
@@ -348,6 +345,7 @@ void toNewConnection::done(int r)
     if (!NewConnection)
         return;
 
+    writeSettings();
     QDialog::done(r);
 }
 
