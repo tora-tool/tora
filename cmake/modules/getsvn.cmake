5b9f8039bee7ffeb37a9378fb30dbff34022007d
ibre5041 <ibre5041@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2012-04-24 16:39:00 +0000

    cmake script swich queries SVN Revision at build time

---
+++ cmake/modules/getsvn.cmake
@@ -0,0 +1,12 @@
+# the FindSubversion.cmake module is part of the standard distribution
+include(FindSubversion)
+# extract working copy information for SOURCE_DIR into MY_XXX variables
+Subversion_WC_INFO(${SOURCE_DIR} MY)
+# write a file with the SVNREVISION define
+file(WRITE svnrevision.h.txt "#define SVNREVISION ${MY_WC_REVISION}\n")
+file(WRITE svnrevision.txt "${MY_WC_REVISION}\n")
+# copy the file to the final header only if the revision changes
+# reduces needless rebuilds
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different svnrevision.h.txt svnrevision.h)
+
+MESSAGE("Root: ${MY_WC_ROOT}")
