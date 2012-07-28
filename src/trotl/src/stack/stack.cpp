f6a833909c16e9ea4acf88a24f78c23a4e2f17b6
ibre5041 <ibre5041@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2012-07-28 16:00:27 +0000

    Sync with project trunk (support for Win64)

--- src/trotl/src/stack/stack.cpp
+++ src/trotl/src/stack/stack.cpp
@@ -3,12 +3,13 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#include <iomanip>
-#include <sstream>
-#include <ostream>
-#include <iostream>
 #include <cassert>
+#include <cstring>
 #include <cstdlib>
+#include <iomanip>
+#include <ostream>
+#include <stdexcept>
+#include <sstream>
 
 #include "stack.hpp"
 
@@ -23,7 +24,6 @@
     #endif
     
 #elif defined(__GNUC__)
-    #include <string.h>
     #include <dlfcn.h>
     #include <cxxabi.h>
 #endif
@@ -35,13 +35,18 @@ namespace
     std::string demangle(const char *name)
     {
         //this just does not work - some QT's private symbols cannot be resolved
-        //if (!name) throws dbg::stack_error then "use -rdynamic";
+        //if (!name) throws std::runtime_error then "use -rdynamic";
         if(!name) return "uknown symbol";
         int status = 0;
         char *d = 0;
         std::string ret = name;
-        try { if ((d = abi::__cxa_demangle(name, 0, 0, &status))) ret = d; }
-        catch(...) {  }
+        try
+        {
+            if ((d = abi::__cxa_demangle(name, 0, 0, &status)))
+                ret = d;
+        }
+        catch (const std::bad_alloc &) {  }
+
         std::free(d);
         return ret;
     }
@@ -49,18 +54,21 @@ namespace
 
 #if defined(_WIN32)
 
+    // Derive from this to disallow copying of your class.
+    // c.f. boost::noncopyable
     class uncopyable
     {
-        public:
+        protected:
             uncopyable() { }
             
         private:
-            uncopyable(const uncopyable &); // remains undefined
-            uncopyable &operator= (const uncopyable &); // remains undefined
+            uncopyable(const uncopyable &);
+            uncopyable &operator= (const uncopyable &);
     };
 
     #if defined(__MINGW32__)
-    
+
+    // Provides a means to translate a program counter offset in to the name of the corresponding function.
     class bfd_context : uncopyable
     {
         private:
@@ -78,12 +86,12 @@ namespace
                 symbol_table_(0)
             {
                 char procname[MAX_PATH];
-                GetModuleFileNameEx(GetCurrentProcess(), NULL, procname, sizeof procname);
+                GetModuleFileNameA(NULL, procname, sizeof procname);
 
                 bfd_init();
                 abfd_ = bfd_openr(procname, 0);
                 if (!abfd_)
-                    throw dbg::stack_error("Failed to parse object data for the executable");
+                    throw std::runtime_error("Failed to parse object data for the executable");
                 
                 char **formats = 0;
                 bool b1 = bfd_check_format(abfd_, bfd_object);
@@ -94,18 +102,18 @@ namespace
                 {
                     bfd_close(abfd_);
                     free(formats);
-                    throw dbg::stack_error("Failed to parse object data for the executable");
+                    throw std::runtime_error("Failed to parse object data for the executable");
                 }
                 free(formats);
                 
                 // Load symbol table
                 unsigned dummy = 0;
-                if (bfd_read_minisymbols(abfd_, FALSE, (void **)&symbol_table_, &dummy) == 0 &&
-                    bfd_read_minisymbols(abfd_, TRUE, (void **)&symbol_table_, &dummy) < 0)
+                if (bfd_read_minisymbols(abfd_, FALSE, reinterpret_cast<void **>(&symbol_table_), &dummy) == 0 &&
+                    bfd_read_minisymbols(abfd_, TRUE, reinterpret_cast<void **>(&symbol_table_), &dummy) < 0)
                 {
                     free(symbol_table_);
                     bfd_close(abfd_);
-                    throw dbg::stack_error("Failed to parse object data for the executable");
+                    throw std::runtime_error("Failed to parse object data for the executable");
                 }
             }
             
@@ -156,19 +164,27 @@ namespace
     
     #endif // __MINGW32__
 
-    class auto_cast_function_ptr
+    // g++ spouts warnings if you use {0} to initialize PODs. So we use this instead:
+    const struct
     {
-        public:
-            template<typename FuncPtr>
-            explicit auto_cast_function_ptr(FuncPtr f) : fptr_(reinterpret_cast<void (*)(void)>(f)) { }
+        template<typename POD>
+        operator POD () const { POD p; std::memset(&p, 0, sizeof p); return p; }
+    }
+    empty_pod = { };
+
+    // Wraps a FARPROC. Implicitly convertible to any kind of pointer-to-function.
+    // Avoids having reinterpret casts all over the place.
+    struct auto_cast_function_ptr
+    {
+		auto_cast_function_ptr(FARPROC f) : fptr_(f) { }
             
             template<typename FuncPtr>
             operator FuncPtr() const { return reinterpret_cast<FuncPtr>(fptr_); }
             
-        private:
-            void (*fptr_)(void);
+			FARPROC fptr_;
     };
 
+    // A wrapper around a DLL. Can dynamically get function pointers with the function() function!
     class windows_dll : uncopyable
     {
         public:
@@ -176,19 +192,19 @@ namespace
                 name_(libname),
                 lib_(LoadLibrary(name_.c_str()))
             {
-                if (!lib_) throw dbg::stack_error("Failed to load dll " + name_);
+                if (!lib_) throw std::runtime_error("Failed to load dll " + name_);
             }
             
             ~windows_dll() { FreeLibrary(lib_); }
             
             const std::string &name() const { return name_; }
-            
-            auto_cast_function_ptr function(const std::string &func_name) const 
-            { 
-                FARPROC proc = GetProcAddress(lib_, func_name.c_str());
-                if (!proc) throw dbg::stack_error("failed to load function " + func_name + " from library " + name_);
-                
-                return auto_cast_function_ptr(proc); 
+ 
+            auto_cast_function_ptr function(const char *func_name) const           
+            {
+                FARPROC proc = GetProcAddress(lib_, func_name);
+                if (!proc) throw std::runtime_error(std::string("failed to load function ") + func_name + " from library " + name_);
+
+                return proc;
             }
         
         private:
@@ -196,79 +212,95 @@ namespace
             HMODULE lib_;
     };
 
+    // An object that makes sure debugging symbols are available
     class symbol_context : uncopyable
     {
         public:
-            symbol_context() { SymInitialize(GetCurrentProcess(), 0, true); }
+            symbol_context()
+            {
+                if (!SymInitialize(GetCurrentProcess(), 0, TRUE))
+                    throw std::runtime_error("Failed to initialize symbol context");
+            }
             ~symbol_context() { SymCleanup(GetCurrentProcess()); }
     };
 
-
+    // A simple Windows mutex class. Use a lock object to lock the mutex for the duration of a scope.
     class mutex : uncopyable
     {
         public:
             mutex() { InitializeCriticalSection(&cs_); }
             ~mutex() { DeleteCriticalSection(&cs_); }
+
+        private:
+            friend class lock;        
             void lock() { EnterCriticalSection(&cs_); }
             void unlock() { LeaveCriticalSection(&cs_); }
-        
-        private:
+
             CRITICAL_SECTION cs_;
-    };
+    }
+    g_fill_frames_mtx;
     
-    class scoped_lock : uncopyable
+    // A lock for the mutex
+    class lock : uncopyable
     {
         public:
-            scoped_lock(mutex &m) : m_(m) { m_.lock(); }
-            ~scoped_lock() { m_.unlock(); }
+            lock(mutex &m) : m_(m) { m.lock(); }
+            ~lock() { m_.unlock(); }
         private:
             mutex &m_;
     };
     
-    mutex fill_frames_mtx_;
-
 
     void fill_frames(std::list<dbg::stack_frame> &frames, dbg::stack::depth_type limit)
     {
-        scoped_lock lk(fill_frames_mtx_);
-        
-        #if defined(__MINGW32__)
-            static bfd_context bfdc;
-        #endif
+        lock lk(g_fill_frames_mtx);
         
         symbol_context sc;
+        #ifdef __MINGW32__
+        bfd_context bfdc;
+        #endif
         
-        STACKFRAME frame; std::memset(&frame, 0, sizeof frame);
-        CONTEXT context;  std::memset(&context, 0, sizeof(CONTEXT));
+        STACKFRAME frame = empty_pod;
+        CONTEXT context = empty_pod;
         context.ContextFlags = CONTEXT_FULL;
 
-        windows_dll kernel32("kernel32.dll");
-        
+        windows_dll kernel32("kernel32.dll");        
         void (WINAPI *RtlCaptureContext_) (CONTEXT*) = kernel32.function("RtlCaptureContext");
         
         RtlCaptureContext_(&context);
-        
+#ifdef _WIN64        
+        frame.AddrPC.Offset = context.Rip;
+        frame.AddrPC.Mode = AddrModeFlat;
+        frame.AddrStack.Offset = context.Rsp;
+        frame.AddrStack.Mode = AddrModeFlat;
+        frame.AddrFrame.Offset = context.Rbp;
+        frame.AddrFrame.Mode = AddrModeFlat;
+#else
         frame.AddrPC.Offset = context.Eip;
         frame.AddrPC.Mode = AddrModeFlat;
         frame.AddrStack.Offset = context.Esp;
         frame.AddrStack.Mode = AddrModeFlat;
         frame.AddrFrame.Offset = context.Ebp;
         frame.AddrFrame.Mode = AddrModeFlat;
+#endif
         
         HANDLE process = GetCurrentProcess();
         HANDLE thread = GetCurrentThread();
         
-        dbg::stack::depth_type skip = 0;
+        bool skip = true;
         bool has_limit = limit != 0;
         char symbol_buffer[sizeof(IMAGEHLP_SYMBOL) + 255];
         char module_name_raw[MAX_PATH];
 
-        while(StackWalk(IMAGE_FILE_MACHINE_I386, process, thread, 
-                        &frame, &context, 0, SymFunctionTableAccess, SymGetModuleBase, 0    ))
+#ifdef _WIN64
+        while(StackWalk(IMAGE_FILE_MACHINE_AMD64, process, thread, &frame, &context, 0, SymFunctionTableAccess, SymGetModuleBase, 0))
+#else
+        while(StackWalk(IMAGE_FILE_MACHINE_I386, process, thread, &frame, &context, 0, SymFunctionTableAccess, SymGetModuleBase, 0))
+#endif
         {
-            if (skip < 1)
+            if (skip)
             {
-                ++skip;
+                skip = false;
                 continue;
             }
             
@@ -277,10 +309,14 @@ namespace
             IMAGEHLP_SYMBOL *symbol = reinterpret_cast<IMAGEHLP_SYMBOL *>(symbol_buffer);
             symbol->SizeOfStruct = (sizeof *symbol) + 255;
             symbol->MaxNameLength = 254;
-            
+
+#ifdef _WIN64            
+            DWORD64 module_base = SymGetModuleBase(process, frame.AddrPC.Offset);
+#else
             DWORD module_base = SymGetModuleBase(process, frame.AddrPC.Offset);
+#endif
             std::string module_name = "[unknown module]";
-            if(   module_base && GetModuleFileName(reinterpret_cast<HINSTANCE>(module_base), module_name_raw, MAX_PATH)) 
+            if (module_base && GetModuleFileNameA(reinterpret_cast<HINSTANCE>(module_base), module_name_raw, MAX_PATH)) 
                 module_name = module_name_raw;
             
             #if defined(__MINGW32__)
@@ -288,23 +324,28 @@ namespace
                 
                 if (func.empty())
                 {
-                    DWORD displacement = 0; // dummy variable
-                    BOOL got_symbol = SymGetSymFromAddr(process, frame.AddrPC.Offset, &displacement, symbol);
+                    DWORD dummy = 0;
+                    BOOL got_symbol = SymGetSymFromAddr(process, frame.AddrPC.Offset, &dummy, symbol);
                     func = got_symbol ? symbol->Name : "[unknown function]";
                 }
             #else
-                DWORD displacement = 0; // dummy variable
-                BOOL got_symbol = SymGetSymFromAddr(process, frame.AddrPC.Offset, &displacement, symbol);
+#ifdef _WIN64
+					DWORD64 dummy = 0;
+#else
+					DWORD dummy = 0;
+#endif                
+                BOOL got_symbol = SymGetSymFromAddr(process, frame.AddrPC.Offset, &dummy, symbol);
                 std::string func = got_symbol ? symbol->Name : "[unknown function]";
             #endif
             
-            dbg::stack_frame f(reinterpret_cast<const void *>(frame.AddrPC.Offset), func + " in " + module_name);
+            dbg::stack_frame f(reinterpret_cast<const void *>(frame.AddrPC.Offset), func, module_name);
             frames.push_back(f);
         }
     }
 #pragma comment(lib, "DBGHELP")
 #elif defined(__GNUC__)
-    #if defined(__i386__) || defined( __x86_64__ )    
+    #if defined(__i386__) || defined(__amd64__)
+
     void fill_frames(std::list<dbg::stack_frame> &frames, dbg::stack::depth_type limit)
     {
         // Based on code found at:
@@ -316,18 +357,18 @@ namespace
         void *ip = frame[1];
         
         bool has_limit = limit != 0;
-        dbg::stack::depth_type skip = 0;
+        bool skip = true;
         
         while(bp && ip && dladdr(ip, &info)) 
         {
-            if (skip < 1)
-                ++skip;
+            if (skip)
+                skip = false;
             else
             {
                 if (has_limit && limit-- == 0) break;
-                frames.push_back(dbg::stack_frame(ip, demangle(info.dli_sname) + " in " + info.dli_fname));
-        
-                if(info.dli_sname && !strcmp(info.dli_sname, "main")) break;
+                frames.push_back(dbg::stack_frame(ip, demangle(info.dli_sname), info.dli_fname));
+
+                if(info.dli_sname && !std::strcmp(info.dli_sname, "main")) break;
             }           
     
             ip = bp[1];
@@ -352,7 +393,7 @@ namespace
             if (has_limit && limit-- == 0) break;
             
             if (dladdr(ip, &info)) 
-                frames.push_back(dbg::stack_frame(ip, demangle(info.dli_sname) + " in " + info.dli_fname));
+                frames.push_back(dbg::stack_frame(ip, demangle(info.dli_sname), info.dli_fname));
             
             if (frame && (frame = static_cast<void**>(*frame))) ip = *(frame + 2);
         }
@@ -360,53 +401,30 @@ namespace
     }
     
     #else
-        // GNU, but neither x86 or PPC
+        // GNU, but not x86, x64 nor PPC
         #error "Sorry but dbg::stack is not supported on this architecture"
     #endif
 #else
     // Unsupported compiler
     #error "Sorry but dbg::stack is not supported on this compiler"
 #endif
-}
+
+} // close anonymous namespace
 
 
 
 namespace dbg
 {
-    stack_error::stack_error(const std::string &what) :
-        what_(what)
-    {
-    }
-    
-    stack_error::~stack_error() throw()
-    {
-    }
+    stack_frame::stack_frame(const void *instruction, const std::string &function, const std::string &module) :
+        instruction(instruction),
+        function(function),
+        module(module)
+		{
+		}
             
-    const char *stack_error::what() const throw()
-    {
-        return what_.c_str();
-    }
-            
-
-    stack_frame::stack_frame(const void *instruction, const std::string &function) :
-        instruction_(instruction),
-        function_(function)
-    {
-    }
-            
-    const void *stack_frame::instruction() const
-    {
-        return instruction_;
-    }
-        
-    const std::string &stack_frame::function() const
-    {
-        return function_;
-    }
-
     std::ostream &operator<< (std::ostream &out, const stack_frame &frame)
     {
-        return out << frame.instruction() << ": " << frame.function();
+        return out << frame.instruction << ": " << frame.function << " in " << frame.module;
     }
 
     stack::stack(depth_type limit)
--- src/trotl/src/stack/stack.hpp
+++ src/trotl/src/stack/stack.hpp
@@ -8,57 +8,50 @@
 
 #include <string>
 #include <list>
-#include <stdexcept>
 #include <iosfwd>
-
 
 namespace dbg
 {
-    class stack_error : public std::exception
+    //! stack_frame objects are collected by a stack object. They contain information about the instruction pointer,
+    //! the name of the corresponding function and the "module" (executable or library) in which the function resides.
+    struct stack_frame
     {
-        public:
-            stack_error(const std::string &what);
-            ~stack_error() throw();
-            
-            const char *what() const throw();
-            
-        private:
-            std::string what_;
+        stack_frame(const void *instruction, const std::string &function, const std::string &module);
+
+        const void *instruction;
+        std::string function;
+        std::string module;
     };
 
-    class stack_frame
-    {
-        public:
-            stack_frame(const void *instruction, const std::string &function);
-            
-            const void *instruction() const;
-            const std::string &function() const;
-            
-        private:
-            const void *instruction_;
-            const std::string function_;
-    };
-    
+    //! Allows you to write a stack_frame object to an std::ostream
     std::ostream &operator<< (std::ostream &out, const stack_frame &frame);
-    
+
+    //! Instantiate a dbg::stack object to collect information about the current call stack. Once created, a stack object
+    //! may be freely copied about and will continue to contain the information about the scope in which collection occurred.
     class stack
     {
         public:
             typedef std::list<stack_frame>::size_type depth_type;
             typedef std::list<stack_frame>::const_iterator const_iterator;
-            
+
+            //! Collect information about the current call stack. Information on the most recent frames will be collected
+            //! up to the specified limit. 0 means unlimited.
+            //! An std::runtime_error may be thrown on failure.
             stack(depth_type limit = 0);
-            
+
+            //! Returns an iterator referring to the "top" stack frame
             const_iterator begin() const;
+
+            //! Returns an iterator referring to one past the "bottom" stack frame
             const_iterator end() const;
-            
+
+            //! Returns the number of frames collected
             depth_type depth() const;
             
         private:
             std::list<stack_frame> frames_;
     };
-    
-    
+
 } // close namespace dbg
 
 #endif // STACK_HPP_0022_01092007
