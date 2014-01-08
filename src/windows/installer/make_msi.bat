d11fd2fffd80bf1c04f425703a1585a192727b30
ibre5041 <ibre5041@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2014-01-08 23:28:14 +0000

    simplified 32/64 bit build

--- src/windows/installer/make_msi.bat
+++ src/windows/installer/make_msi.bat
@@ -1,5 +1,13 @@
-"c:\Program Files\WiX Toolset v3.7\bin\candle.exe" tora.wxs
-"c:\Program Files\WiX Toolset v3.7\bin\light.exe" -ext WixUIExtension -o tora3alpha.32bit.msi tora.wixobj
+@echo off
+SET BUILD_ARCH=x86
+
+SET DIR1=c:\Program Files (x86)\WiX Toolset v3.7\bin
+SET DIR2=c:\Program Files\WiX Toolset v3.7\bin
+
+IF EXIST "%DIR1%"\ SET PATH=%DIR1%;%PATH%
+IF EXIST "%DIR2%"\ SET PATH=%DIR2%;%PATH%
+
+candle.exe tora.wxs
+light.exe -ext WixUIExtension -o tora3alpha.32bit.msi tora.wixobj
 
 @pause
-
