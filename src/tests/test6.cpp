8c24be75431786df10bdc89328a68ad62177f064
ibre5041 <ibre5041@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2014-04-19 22:47:05 +0000

    Parsing test for .dot grammar

--- src/tests/test6.cpp
+++ src/tests/test6.cpp
@@ -32,17 +32,76 @@
  *
  * END_COMMON_COPYRIGHT_HEADER */
 
+#include "dotgrammar.h"
+#include "dotgraph.h"
+#include "DotGraphParsingHelper.h"
+
+#include <boost/spirit/include/classic_confix.hpp>
+#include <boost/throw_exception.hpp>
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+#include <QtCore/QString>
+
 #include <stdlib.h>
 #include <stdio.h>
+
+using namespace std;
+
+static void usage();
+string slurp(string const& fileName);
+
+extern DotGraphParsingHelper* phelper;
+
+int main(int argc, char **argv)
+{
+	using namespace boost::spirit::classic;
+	if (argc != 2)
+		usage();
+
+	string txt = slurp(string(argv[1]));
+
+	cout << txt << endl;
+
+	DotGraph newGraph(QString("xdot"), QString(argv[1]));
+	phelper = new DotGraphParsingHelper;
+	phelper->graph = &newGraph;
+	phelper->z = 1;
+	phelper->maxZ = 1;
+	phelper->uniq = 0;
+
+	DotGrammar g;
+	//parse_info<> p_info= boost::spirit::classic::parse(txt.c_str(), g >> end_p, (+boost::spirit::classic::space_p|boost::spirit::classic::comment_p("/*", "*/")));
+	parse_info<> p_info= boost::spirit::classic::parse(txt.c_str()
+//			"[_draw_=\"c 9 -#fffffe00 C 7 -#ffffff P 4 0 0 0 36 54 36 54 0 \","
+//            "bb=\"0,0,54,36\","
+//            "fontsize=11, xdotversion=1.6,"
+//            "id=unnamed]"
+//            "xdotversion=1.6"
+			, g >> end_p, (+boost::spirit::classic::space_p|boost::spirit::classic::comment_p("/*", "*/")));
+
+	if (p_info.full)
+		cout << "Parse OK" << endl;
+	else
+		cout << "Parse Fail at:" << p_info.length << " " << p_info.stop << endl;
+}
 
 static void usage()
 {
 	printf("Usage:\n\n  test6 filename\n\n");
-	exit(2); 
+	exit(2);
 }
 
-int main(int argc, char **argv)
+string slurp(string const& fileName)
 {
-	usage();
-}
+        ifstream ifs(fileName.c_str(), ios::in | ios::binary | ios::ate);
+        ifstream::pos_type fileSize = ifs.tellg();
+        ifs.seekg(0, ios::beg);
 
+        stringstream sstr;
+        sstr << ifs.rdbuf();
+        return sstr.str();
+}
