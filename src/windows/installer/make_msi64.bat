eb54612c127673a9f85555c2b65f289e9a8672f3
ibre5041 <ibre5041@4a83093e-0cc6-42a4-b7cd-3300864c03d0>
2014-07-18 09:41:46 +0000

    new wix version

--- src/windows/installer/make_msi64.bat
+++ src/windows/installer/make_msi64.bat
@@ -1,8 +1,8 @@
 @echo off
 SET BUILD_ARCH=x64
 
-SET DIR1=c:\Program Files (x86)\WiX Toolset v3.7\bin
-SET DIR2=c:\Program Files\WiX Toolset v3.7\bin
+SET DIR1=c:\Program Files (x86)\WiX Toolset v3.8\bin
+SET DIR2=c:\Program Files\WiX Toolset v3.8\bin
 
 IF EXIST "%DIR1%"\ SET PATH=%DIR1%;%PATH%
 IF EXIST "%DIR2%"\ SET PATH=%DIR2%;%PATH%
