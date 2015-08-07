dc96dea1fed39afdac05926f68d46ec4466854cc
ibre5041 <ibre5041@ibrezina.net>
2015-08-07 08:02:20 +0200

    Use git to retreive windows minor release number

--- src/windows/installer/make_msi64_qt5.bat
+++ src/windows/installer/make_msi64_qt5.bat
@@ -9,8 +9,15 @@ IF EXIST "%DIR2%"\ SET PATH=%DIR2%;%PATH%
 
 set BUILD_NUMBER=
 
-for /F "tokens=1,2"  %%t  in ('svn info') do @if "%%t"=="Revision:" set BUILD_NUMBER=%%u
+REM for /F "tokens=1,2"  %%t  in ('svn info') do @if "%%t"=="Revision:" set BUILD_NUMBER=%%u
+REM echo Build Number: %BUILD_NUMBER%
 
+
+for /F "tokens=1"  %%t  in ('git describe --long --tags --dirty --always') do set GIT_RELEASE=%%t
+REM set GIT_RELEASE=v3.0alpha-30-g8e691f2-dirty
+echo %GIT_RELEASE%
+
+for /f "tokens=2 delims=- " %%G IN ("%GIT_RELEASE%") DO set BUILD_NUMBER=%%G
 echo Build Number: %BUILD_NUMBER%
 
 candle.exe tora-qt5.wxs
