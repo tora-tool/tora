3d877308c56d726257794119856d8f896520099e
ibre5041 <ibre5041@ibrezina.net>
2016-02-20 23:40:30 +0100

    boost 1.48 blacklisted, use 1.55

--- travis/install.sh
+++ travis/install.sh
@@ -9,7 +9,7 @@ if [ "$QTTYPE" = "5" ]; then
 fi
 
 sudo apt-get purge libboost*1.46*
-sudo apt-get install libboost1.48-dev
+sudo apt-get install libboost1.55-dev
 sudo apt-get install libqscintilla2-dev cmake libloki-dev xvfb g++-4.9
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 80 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 #sudo update-alternatives --config gcc
