fb1c3c9ca64b0e2f86372c6e7acf5399c6b3eace
ibre5041 <ibre5041@ibrezina.net>
2016-02-20 22:51:19 +0100

    liber depends on boost

--- travis/install.sh
+++ travis/install.sh
@@ -8,6 +8,6 @@ if [ "$QTTYPE" = "5" ]; then
 	sudo apt-get install -y ubuntu-sdk qtquick1-5-dev 
 fi
 
-sudo apt-get install libboost-all-dev libqscintilla2-dev cmake libloki-dev xvfb g++-4.9
+sudo apt-get install libboost-all-dev libqscintilla2-dev cmake libloki-dev xvfb g++-4.9 libboost-dev
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 80 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 #sudo update-alternatives --config gcc
