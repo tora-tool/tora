ec4815a98fae9c8c2856945d1717bdef9d5ff2d4
ibre5041 <ibre5041@ibrezina.net>
2016-02-20 23:22:41 +0100

    travis boost install

--- travis/install.sh
+++ travis/install.sh
@@ -8,6 +8,9 @@ if [ "$QTTYPE" = "5" ]; then
 	sudo apt-get install -y ubuntu-sdk qtquick1-5-dev 
 fi
 
-sudo apt-get install libboost-all-dev libqscintilla2-dev cmake libloki-dev xvfb g++-4.9 libboost-dev
+sudo apt-get update
+sudo apt-get purge libboost*1.46*
+sudo apt-get install libboost-all-dev
+sudo apt-get install libqscintilla2-dev cmake libloki-dev xvfb g++-4.9
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 80 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 #sudo update-alternatives --config gcc
