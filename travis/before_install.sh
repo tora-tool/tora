31cae185190322c7374699555184230ec1c39017
ibre5041 <ibre5041@ibrezina.net>
2016-03-11 23:46:21 +0100

    Travis build fix. Try to use newer version of Qt5 (5.0.2)

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+#sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+sudo apt-add-repository --yes ppa:beineri/opt-qt542
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository --yes ppa:kalakris/cmake
 sudo apt-get update -qq
@@ -9,9 +10,8 @@ cd /tmp/pk
 wget http://petr.insw.cz/repo/oracle-instantclient12.1-basic_12.1.0.2.0-2_amd64.deb
 wget http://petr.insw.cz/repo/oracle-instantclient12.1-devel_12.1.0.2.0-2_amd64.deb
 sudo dpkg -i *
-# nasty patch
+# nasty patch (cmake 3.0.x required on OSX)
 cd /home/travis/build/tora-tool/tora
 sed -i -e 's/VERSION 2.8.11/VERSION 2.8.9/g;s/CMAKE_POLICY/#CMAKE_POLICY/' CMakeLists.txt
 sed -i -e 's/VERSION 2.8.11/VERSION 2.8.9/g;s/CMAKE_POLICY/#CMAKE_POLICY/' extlibs/trotl/CMakeLists.txt
-mv /tmp/cmake CMakeLists.txt
 echo Patched cmake file
