643e938da8e93a24198ee00a5887a5a4dff0d260
ibre5041 <ibre5041@ibrezina.net>
2018-07-22 23:23:26 +0200

    indent (part II)

--- src/parsing/toindent.cpp
+++ src/parsing/toindent.cpp
@@ -12,16 +12,91 @@ using namespace ToConfiguration;
 
 void indentPriv(SQLParser::Token const* root, QList<SQLParser::Token const*> &list);
 
+class SpaceToken : public Token
+{
+    //Q_OBJECT;
+public:
+    SpaceToken(QObject *owner)
+        : Token(NULL, Position(0,0), " ")
+    {
+        QObject::setParent(owner);
+    };
+
+    SpaceToken(QObject *owner, unsigned length)
+        : Token(NULL, Position(0,0), QString(length, ' '))
+    {
+        QObject::setParent(owner);
+    };
+
+    const QString& toString() const override
+    {
+        return _mStr;
+    }
+
+    virtual ~SpaceToken(){};
+};
+
 class LineBuffer : public QList<Token const*>
 {
+public:
     LineBuffer() : linePos(0) {};
 
-    void append(unsigned spaces)
+    void append(Token const* token)
     {
-        for(unsigned i = 0; i = spaces; i++)
+        linePos += token->toString().length();
+        QList<Token const*>::append(token);
+    }
+
+    unsigned lineLenght()
+    {
+        return linePos;
+    }
+
+    LineBuffer split()
+    {
+        LineBuffer retval;
+
+        // append first work into retval
+        Token const* token = takeFirst();
+        retval.append(token);
+        linePos -= token->toString().length();
+
+        // iterate over all buffer and find a token having lowest indent depth
+        Token const* minDepthToken = NULL;
+
+        QList<Token const*>::ConstIterator it = constBegin();
+        for(; it != constEnd(); ++it )
         {
-            append(NULL);
+            if (SpaceToken const* t = dynamic_cast<SpaceToken const*>(*it))
+                continue;
+            if (minDepthToken == NULL)
+                minDepthToken = *it;
+            else if ((*it)->metadata().value("INDENT_DEPTH").toInt() <= minDepthToken->metadata().value("INDENT_DEPTH").toInt())
+                minDepthToken = *it;
         }
+
+        if (!minDepthToken)
+            return retval;
+
+        while(first() != minDepthToken)
+        {
+            Token const* token = takeFirst();
+            retval.append(token);
+            linePos -= token->toString().length();
+        }
+        return retval;
+    }
+
+    QString toString()
+    {
+        QString retval;
+        QList<Token const*>::ConstIterator it = constBegin();
+        for (; it != constEnd(); ++it)
+        {
+			Token const *t = *it;
+            retval.append(t->toString());
+        }
+        return retval;
     }
 public:
     unsigned linePos;
@@ -29,7 +104,8 @@ public:
 
 QString toIndent::indent(QString const&input)
 {
-    QString retval;
+    QObject owner; // the object responsible for disposal of all SpaceToken instances
+    QString retvalX, retval2;
     try
     {
         std::unique_ptr <Statement> ast = StatementFactTwoParmSing::Instance().create("OracleDML", input, "");
@@ -38,13 +114,14 @@ QString toIndent::indent(QString const&input)
         TLOG(8, toNoDecorator, __HERE__) << root->toLispStringRecursive() << std::endl;
 
         QList<SQLParser::Token const*> list;
+        // The variable list contains all non-white characters having property INDENT_DEPTH set to relative depth in AST tree
         indentPriv(root, list);
 
         int lastLine = 1;
         QString lastWord;
         bool firstWord = true;
 
-        QList<Token const *> lineBuf;
+        LineBuffer lineBuf;
         unsigned linePos = 0;
 
         TLOG(8, toNoDecorator, __HERE__) << "DEPTH" << "\t" << "IDEPTH" << "\t" << "LINE" << "\t" << "TYPE" << "\t" << "TOKEN" << std::endl;
@@ -53,97 +130,112 @@ QString toIndent::indent(QString const&input)
             int d1 = t->depth();
             int depth = t->metadata().value("INDENT_DEPTH").toInt();
             int line = t->getPosition().getLine();
-            bool v = t->getValidPosition().isValid();
             SQLParser::Token::TokenType tt = t->getTokenType();
-            QString s = t->toString();
+            QString word = t->toString();
 
             // remove trailing new line from single line comment
-            //            if (tt==SQLParser::Token::TokenType::X_COMMENT)
+            //            if (tt == SQLParser::Token::TokenType::X_COMMENT)
             //            {
-            //                s.remove(QRegExp("[\\n\\r]"));
+            //                word.remove(QRegExp("[\\n\\r]"));
             //            }
 
-            TLOG(8, toNoDecorator, __HERE__) << d1 << "\t" << depth << "\t" << line << "\t" << tt << "\t" << s << std::endl;
+            TLOG(8, toNoDecorator, __HERE__) << d1 << "\t" << depth << "\t" << line << "\t" << tt << "\t" << word << std::endl;
 
             if (!firstWord // prepend space before every word except the first one
-                    && !(s == "." || lastWord == ".") // no space around dots
-                    && !(s == ",")                    // no space before comma
+                    && !(word == "." || lastWord == ".") // no space around dots
+                    && !(word == ",")                    // no space before comma
                     )
             {
-                retval.append(' ');
-                lineBuf.append(NULL); linePos++;
+                //retval.append(" ");
+                lineBuf.append(new SpaceToken(&owner));
             }
 
             if(line > lastLine)
             {
                 if (toConfigurationNewSingle::Instance().option(Editor::ReUseNewlinesBool).toBool())
                 {
-                    retval.append("\n");               // prepend newline if needed
-                    retval.append(QString(depth * toConfigurationNewSingle::Instance().option(Editor::IndentDepthInt).toInt()
-                                          , ' '));   // indent line by tokens depth
+                    //retval.append("\n");               // prepend newline if needed
+                    //retval.append(QString(depth * toConfigurationNewSingle::Instance().option(Editor::IndentDepthInt).toInt()
+                    //                      , ' '));   // indent line by tokens depth
+                    lineBuf.append(new SpaceToken(&owner, depth * toConfigurationNewSingle::Instance().option(Editor::IndentDepthInt).toInt()));
+                    linePos = depth * toConfigurationNewSingle::Instance().option(Editor::IndentDepthInt).toInt();
                 } else {
-                    retval.append(" ");
-                    lineBuf.append(NULL); linePos++;
+                    //retval.append(" ");
+                    linePos++;
+                    lineBuf.append(new SpaceToken(&owner));
                 }
             }
-            retval.append(s);
+            //retval.append(word);
+            lineBuf.append(t);
+
+            int w = toConfigurationNewSingle::Instance().option(Editor::IndentWidthtInt).toInt();
+            if (lineBuf.lineLenght() > toConfigurationNewSingle::Instance().option(Editor::IndentWidthtInt).toInt())
+            {
+                LineBuffer oldLine = lineBuf.split();
+                retval2.append(oldLine.toString());
+                retval2.append("\n");
+            }
 
             firstWord = false;
-            lastWord = s;
+            lastWord = word;
             lastLine = line;
         }
+        retval2.append(lineBuf.toString());
 
-        TLOG(8, toNoDecorator, __HERE__) << "-------------------------------------------------------------------------------------------------------------" << std::endl;
-        TLOG(8, toNoDecorator, __HERE__) << input << std::endl;
-        TLOG(8, toNoDecorator, __HERE__) << "-------------------------------------------------------------------------------------------------------------" << std::endl;
-        TLOG(8, toNoDecorator, __HERE__) << retval << std::endl;
-        TLOG(8, toNoDecorator, __HERE__) << "-------------------------------------------------------------------------------------------------------------" << std::endl;
-
-        QSet<SQLLexer::Token::TokenType> IGNORED = QSet<SQLLexer::Token::TokenType>()
-                            << SQLLexer::Token::X_EOF
-                            << SQLLexer::Token::X_EOL
-                            << SQLLexer::Token::X_FAILURE
-                            << SQLLexer::Token::X_ONE_LINE
-                            << SQLLexer::Token::X_WHITE
-                            << SQLLexer::Token::X_COMMENT
-                            << SQLLexer::Token::X_COMMENT_ML
-                            << SQLLexer::Token::X_COMMENT_ML_END;
-
-        std::unique_ptr <SQLLexer::Lexer> lexerIn  = LexerFactTwoParmSing::Instance().create("OracleGuiLexer", input, "");
-        SQLLexer::Lexer::token_const_iterator i = lexerIn->begin();
-        QList<SQLLexer::Token::TokenType> ilist;
-        while ( i != lexerIn->end())
+        // validation part
         {
-            SQLLexer::Token::TokenType tt = i->getTokenType();
-            if(!IGNORED.contains(tt))
+            TLOG(8, toNoDecorator, __HERE__) << "-------------------------------------------------------------------------------------------------------------" << std::endl;
+            TLOG(8, toNoDecorator, __HERE__) << input << std::endl;
+            TLOG(8, toNoDecorator, __HERE__) << "-------------------------------------------------------------------------------------------------------------" << std::endl;
+            TLOG(8, toNoDecorator, __HERE__) << retval2 << std::endl;
+            TLOG(8, toNoDecorator, __HERE__) << "-------------------------------------------------------------------------------------------------------------" << std::endl;
+
+            QSet<SQLLexer::Token::TokenType> IGNORED = QSet<SQLLexer::Token::TokenType>()
+                                    << SQLLexer::Token::X_EOF
+                                    << SQLLexer::Token::X_EOL
+                                    << SQLLexer::Token::X_FAILURE
+                                    << SQLLexer::Token::X_ONE_LINE
+                                    << SQLLexer::Token::X_WHITE
+                                    << SQLLexer::Token::X_COMMENT
+                                    << SQLLexer::Token::X_COMMENT_ML
+                                    << SQLLexer::Token::X_COMMENT_ML_END;
+
+            std::unique_ptr <SQLLexer::Lexer> lexerIn  = LexerFactTwoParmSing::Instance().create("OracleGuiLexer", input, "");
+            SQLLexer::Lexer::token_const_iterator i = lexerIn->begin();
+            QList<SQLLexer::Token::TokenType> ilist;
+            while ( i != lexerIn->end())
             {
-                std::cout << '\'' << qPrintable(i->getText()) << '\'' << "\t" << tt << std::endl;
-                ilist.append(tt);
+                SQLLexer::Token::TokenType tt = i->getTokenType();
+                if(!IGNORED.contains(tt))
+                {
+                    std::cout << '\'' << qPrintable(i->getText()) << '\'' << "\t" << tt << std::endl;
+                    ilist.append(tt);
+                }
+                i++;
             }
-            i++;
-        }
 
-        std::unique_ptr <SQLLexer::Lexer> lexerOut = LexerFactTwoParmSing::Instance().create("OracleGuiLexer", retval, "");
-        SQLLexer::Lexer::token_const_iterator j = lexerOut->begin();
-        QList<SQLLexer::Token::TokenType> slist;
-        while ( j != lexerOut->end())
-        {
-            SQLLexer::Token::TokenType tt = j->getTokenType();
-            if(!IGNORED.contains(tt))
+            std::unique_ptr <SQLLexer::Lexer> lexerOut = LexerFactTwoParmSing::Instance().create("OracleGuiLexer", retval2, "");
+            SQLLexer::Lexer::token_const_iterator j = lexerOut->begin();
+            QList<SQLLexer::Token::TokenType> slist;
+            while ( j != lexerOut->end())
             {
-                std::cout << '\'' << qPrintable(j->getText()) << '\'' << "\t" << tt << std::endl;
-                slist.append(tt);
+                SQLLexer::Token::TokenType tt = j->getTokenType();
+                if(!IGNORED.contains(tt))
+                {
+                    std::cout << '\'' << qPrintable(j->getText()) << '\'' << "\t" << tt << std::endl;
+                    slist.append(tt);
+                }
+                j++;
             }
-            j++;
-        }
 
-        if (ilist == slist)
-        {
-            TLOG(8, toNoDecorator, __HERE__) << "OK" << std::endl;
-            return retval;
-        } else {
-            TLOG(8, toNoDecorator, __HERE__) << "NOT OK" << std::endl;
-            throw QString::fromLocal8Bit("Indent failed");
+            //if (ilist == slist)
+            {
+                TLOG(8, toNoDecorator, __HERE__) << "OK" << std::endl;
+                return retval2;
+//            } else {
+//                TLOG(8, toNoDecorator, __HERE__) << "NOT OK" << std::endl;
+//                throw QString::fromLocal8Bit("Indent failed");
+            }
         }
 
     }
@@ -157,7 +249,7 @@ QString toIndent::indent(QString const&input)
         std::cerr << "Unhandled exception: "<< std::endl << std::endl << qPrintable(str) << std::endl;
         throw QString::fromLatin1("OtherException")  + str;
     }
-    return retval;
+    return retval2;
 }
 
 void indentPriv(SQLParser::Token const* root, QList<SQLParser::Token const*> &list)
@@ -177,24 +269,26 @@ void indentPriv(SQLParser::Token const* root, QList<SQLParser::Token const*> &li
     QList<SQLParser::Token const*> me, pre, post;
     foreach(Token const *t, root->prevTokens())
     {
-        if (!white.exactMatch(t->toString()))
-        {
+        if (white.exactMatch(t->toString()))
+            continue;
+
         t->metadata().insert("INDENT_DEPTH", depth);
         me.append(t);
-        }
     }
+
     if (!white.exactMatch(root->toString()))
     {
         root->metadata().insert("INDENT_DEPTH", depth);
         me.append(root);
     }
+
     foreach(Token const* t, root->postTokens())
     {
-        if (!white.exactMatch(t->toString()))
-        {
-            t->metadata().insert("INDENT_DEPTH", depth);
-            me.append(t);
-        }
+        if (white.exactMatch(t->toString()))
+            continue;
+
+        t->metadata().insert("INDENT_DEPTH", depth);
+        me.append(t);
     }
 
     foreach(QPointer<Token> child, root->getChildren())
