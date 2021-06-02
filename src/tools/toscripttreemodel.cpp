2d7fea4335fe6889bff69d5fb0f065e4732d1e9c
Ivan Brezina <Ivan.Brezina@sap.com>
2021-06-02 16:36:30 +0200

    qt6 core - fixup Qt enums (Part 25)

--- src/tools/toscripttreemodel.cpp
+++ src/tools/toscripttreemodel.cpp
@@ -77,7 +77,7 @@ QVariant toScriptTreeModel::data(const QModelIndex &index, int role) const
 Qt::ItemFlags toScriptTreeModel::flags(const QModelIndex &index) const
 {
     if (!index.isValid())
-        return 0;
+        return Qt::NoItemFlags;
 
     return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
 }
