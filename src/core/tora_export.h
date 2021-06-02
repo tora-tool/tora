2cc57c9f2a030dccf3c6ecefd88cef96f2911364
Ivan Brezina <Ivan.Brezina@sap.com>
2021-06-02 13:28:55 +0200

    qt6 core - umplaut "fix" - MSVC2019 does not like it (Part 19 )

--- src/core/tora_export.h
+++ src/core/tora_export.h
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 // The Loki Library
-// Copyright (c) 2006 Peter Kümmel
+// Copyright (c) 2006 Peter Kummel
 // Permission to use, copy, modify, distribute and sell this software for any
 //     purpose is hereby granted without fee, provided that the above copyright
 //     notice appear in all copies and that both that copyright notice and this
